3485c9ac55dcab6a67e830d3acb15c03df05f288
gudrmsglgl <gudrmsglgl@gmail.com>
2022-07-09 15:56:46 +0900

    refactor 함수 추출(6.1)

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,27 +1,8 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
-
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
-
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+  printBanner();
+  let outstanding = calculateOutStanding(invoice);
+  recordDueDate(invoice);
+  printDetail(invoice, outstanding);
 }
 
 const invoice = {
@@ -29,3 +10,28 @@ const invoice = {
   customer: '엘리',
 };
 printOwing(invoice);
+
+function printBanner() {
+  console.log('***********************');
+  console.log('**** Customer Owes ****');
+  console.log('***********************');
+}
+
+function calculateOutStanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+function recordDueDate(invoice) {
+  const today = new Date();
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+}
+
+function printDetail(invoice, outstanding) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
