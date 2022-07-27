67659ed80fb3f6101c967e1b51aad21257de0165
gudrmsglgl <gudrmsglgl@gmail.com>
2022-07-27 23:59:37 +0900

    리팩토링 후 - 레코드 캡슐화

---
+++ ch7/7-1.js
@@ -0,0 +1,34 @@
+const organization = new Organization({ 
+    name: 'Acme Gooseberries', 
+    country: 'GB' 
+});
+
+organization.name = 'Dream Coding';
+console.log(organization.name);
+console.log(organization.country);
+
+class Organization {
+    #name;
+    #country;
+    constructor(data) {
+        this.#name = data.name;
+        this.#country = data.country;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    set country(value) {
+        this.#country = value;
+    }
+
+}
