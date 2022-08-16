e0f254ea2b77e66d736149d999212502aa8fc8a9
gudrmsglgl <gudrmsglgl@gmail.com>
2022-08-16 15:04:56 +0900

    "값을 참조로 바꾸기 - 자주 바뀌는 객체의 통일성을 유지하기 위해" - 불변성은 안전성은 좋으나 > 매번 새로운 인스턴스를 생성했을 때 예전 데이터가 현재 반영된 데이터를 따라오지 못하는 경우가 있다. - 이럴 때 참조를 사용한다. - 고유한 아이디별로 하나의 인스턴스를 만드는것을 보장하고 싶으면 repository 패턴을 사용한다.

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,11 @@
+const customerRepository = new CustomerRepository();
+const order = new Order(data.number, customerRepository.findCustomer(data.customerId));
+
 class Order {
-  constructor(data) {
-    this._number = data.number;
-    this._customer = new Customer(data.customerId);
+  // customer를 의존성 주입
+  constructor(number, customer) {
+    this._number = number;
+    this._customer = customer;
   }
 
   get customer() {
@@ -17,4 +21,24 @@ class Customer {
   get id() {
     return this._id;
   }
+}
+
+class CustomerRepository {
+  #customers;
+
+  constructor() {
+    this.#customers = new Map();
+  }
+
+  registerCustomer(id) {
+    if (!this.#customers.has(id)) {
+      this.#customers.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+
+  findCustomer(id) {
+    return this.#customers.get(id);
+  }
+
 }
