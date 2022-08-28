b2e3a2e8be72f67e4393823217a4d48773c39dc3
gudrmsglgl <gudrmsglgl@gmail.com>
2022-08-28 11:57:42 +0900

    특이 케이스 추가하기

--- ch10/10-5.js
+++ ch10/10-5.js
@@ -1,10 +1,20 @@
+import { extend } from 'lodash';
+
 export class Site {
   constructor(customer) {
     this._customer = customer;
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === 'unknown'
+     ? new UnknownCustomer()
+     : new Customer(this._customer);
+  }
+}
+
+class UnknownCustomer extends Customer {
+  get name() {
+    return 'occupant';
   }
 }
 
@@ -33,10 +43,5 @@ export class Customer {
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
-  // 더 많은 코드가 여기에 있음
-  let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
-  else customerName = aCustomer.name;
-
-  return customerName;
+  return aCustomer.name;
 }
