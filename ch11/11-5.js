0b3ad257dd817e8a3be0bc9073880881afd822ca
gudrmsglgl <gudrmsglgl@gmail.com>
2022-09-08 19:55:25 +0900

    "매개변수를 질의 함수로 바꾸기 - 11.5.js" - 웬만하면 매겨변수를 없애는 것이 좋음 - 질의함수로 변경하여 사용

---
+++ ch11/11-5.js
@@ -0,0 +1,27 @@
+export class Order {
+  constructor(quantity, itemPrice) {
+    this.quantity = quantity;
+    this.itemPrice = itemPrice;
+  }
+
+  get finalPrice() {
+    return this.discountedPrice();
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  #discountedPrice() {
+    switch (this.discountLevel) {
+      case 1:
+        return this.basePrice * 0.95;
+      case 2:
+        return this.basePrice * 0.9;
+    }
+  }
+}
