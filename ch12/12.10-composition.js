39a8b03182988fd12ec13e74efb367a06cabb99b
gudrmsglgl <gudrmsglgl@gmail.com>
2022-09-17 21:27:00 +0900

    "상속 > 컴포지션으로 대체" - 딜리게이터(객체) 해당 메소드를 대신하여 실행

---
+++ ch12/12.10-composition.js
@@ -0,0 +1,34 @@
+// 컴포지션(위임)
+class Printer {
+    #printerHeader;
+    constructor(printerHeader) {
+        this.#printerHeader = printerHeader;
+    }
+    print() {
+        this.#printerHeader
+            ? this.#printerHeader.print()
+            : console.log('기본적인 출력!');
+    }
+}
+
+
+class RedPrinterHeader {
+    print() {
+        console.log('🛑 출력!');
+    }
+}
+
+class BlackPrinterHeader {
+    print() {
+        console.log('⚫️ 출력!')
+    }
+}
+
+class BluePrinterHeader {
+    print () {
+        console.log('🔵 출력!')
+    }
+}
+
+const printers = [new Printer(), new Printer(new RedPrinterHeader()), new Printer(new BluePrinterHeader()), new Printer(new BlackPrinterHeader())];
+printers.forEach((printer) => printer.print());
